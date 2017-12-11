d0fdeb0cd3f937e2f242fb03a4868f63a3c6707a
Filipp Zhuravlev <shiningfinger@list.ru>
2017-12-11 14:43:43 +0300

    Add default TimeStamped model and global settings for a site.

--- bussines/app/admin.py
+++ bussines/app/admin.py
@@ -1,4 +1,48 @@
 # -*- encoding: utf-8 -*-
 from django.contrib import admin
+from singlemodeladmin import SingleModelAdmin
+from .models import *
 
-# Register your models here.
+@admin.register(Settings)
+class SettingsAdmin(SingleModelAdmin):
+    fieldsets = (
+        ('Настройка информации сайта', {
+            'fields': (
+                ('widgets',),
+                ('meta',)
+            ),
+        },),
+        ('Настройка контактной информации', {
+            'fields': (
+                ('email', 'footer_phone',),
+                ('city', 'address',),
+                ('addressHref',),
+            ),
+        },),
+        ('Ссылки', {
+            'fields': (
+                ('default_link_color', 'default_link_hover_color',),
+            ),
+        },),
+        ('Загрузка', {
+            'fields': (
+                ('preloader_color', 'curtain_bg',),
+            ),
+        },),
+        ('Фон сайта', {
+            'fields': (
+                ('default_bg', 'default_image_bg',),
+            ),
+        },),
+        ('Шрифт', {
+            'fields': (
+                ('default_color',),
+            ),
+        },),
+        ('Нижняя часть страницы', {
+            'fields': (
+                ('footer_photo',),
+                ('footer_copyright',)
+            ),
+        },),
+    )
--- bussines/app/models.py
+++ bussines/app/models.py
@@ -2,6 +2,9 @@
 from django.db import models
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+from colorfield.fields import ColorField
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFit
 
 if not settings.DEBUG:
     encoding = 'utf-8'
@@ -9,4 +12,111 @@ if not settings.DEBUG:
     reload(sys)
     sys.setdefaultencoding(encoding)
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+
+class Settings(TimeStampedModel):
+    name = models.CharField(
+        _('Имя настройки'),
+        max_length=100,
+        default='Глобальная настройка'
+    )
+    widgets = models.TextField(
+        _('Метрики, виджеты и прочее'),
+        max_length=8196,
+        blank=True,
+        null=True
+    )
+    meta = models.TextField(
+        _('Глобальное META-описание сайта'),
+        max_length=300,
+        blank=True,
+        null=True
+    )
+    email = models.CharField(
+        _('Email'),
+        max_length=200,
+        blank=True,
+        null=True
+    )
+    footer_phone = models.CharField(
+        _('Телефон в нижней части страници'),
+        max_length=90,
+        blank=True,
+        null=True,
+        default='+7 (343) 111-11-11'
+    )
+    city = models.CharField(
+        _('Город'),
+        max_length=100,
+        blank=True,
+        null=True,
+        default='г. Екатеринбург'
+    )
+    address = models.CharField(
+        _('Адрес'),
+        max_length=200,
+        blank=True,
+        null=True,
+        default='ул. Первомайская, 77'
+    )
+    addressHref = models.CharField(
+        _('Ссылка перенаправляющая на карты(Google/Yandex)'),
+        max_length=500,
+        blank=True,
+        null=True,
+        default='https://www.google.ru/maps/place/%D1%83%D0%BB.+%D0%9F%D0%B5%D1%80%D0%B2%D0%BE%D0%BC%D0%B0%D0%B9%D1%81%D0%BA%D0%B0%D1%8F,+77,+%D0%95%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3,+%D0%A1%D0%B2%D0%B5%D1%80%D0%B4%D0%BB%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB.,+620062/@56.8459859,60.639357,17z/data=!3m1!4b1!4m5!3m4!1s0x43c16c2be5298e21:0x77522d799d8f3f75!8m2!3d56.8459859!4d60.6415457'
+    )
+    footer_photo = models.ForeignKey(
+        "album.album",
+        verbose_name=_('Альбом'),
+        related_name='footer_album',
+        blank=True,
+        null=True
+    )
+
+    footer_copyright = models.CharField(
+        _('Текст копирайта в нижней части страницы'),
+        max_length=20,
+        blank=True,
+        null=True,
+        default='Copyright by Ideal\''
+    )
+
+    default_link_color = ColorField(_('Стандартный цвет ссылок'), blank=True,
+                                    null=True)
+    default_link_hover_color = ColorField(_('Стандартный цвет ссылок при наведении'), blank=True,
+                                          null=True)
+
+    default_color = ColorField(_('Цвет шрифта по умолчанию'), blank=True,
+                               null=True)
+    default_bg = ColorField(_('Цвет фона сайта'), blank=True,
+                            null=True)
+    default_image_bg = ProcessedImageField(
+        verbose_name=_('Фоновое изображение сайта'),
+        upload_to='background',
+        processors=[ResizeToFit(1280)],
+        format='JPEG',
+        options={'quality': 90},
+        blank=True,
+        null=True
+
+    )
+
+    preloader_color = ColorField(_('Цвет прелоадера'), blank=True,
+                                 null=True)
+    curtain_bg = ColorField(_('Цвет фона предварительной загрузки'), blank=True,
+                            null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table='site_settings'
+        verbose_name = _('Настройка')
+        verbose_name_plural = _('Настройки')
+
